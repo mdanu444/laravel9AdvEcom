3b62385abbe0c73b84c2a6ce608aece52a0fd114
mdanu444 <mdanu444@gmail.com>
2022-11-04 18:34:53 +0600

    AdminLTE Installed

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\SiteIdentity;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $siteData = SiteIdentity::first();
+        $siteTitle = !empty($siteData->title) ? $siteData->title : "Adv. Ecommerce";
+        $sitelogo = !empty($siteData->logo) ? $siteData->logo : "dist/img/AdminLTELogo.png";
+        $footer_copyright = !empty($siteData->footer_copyright) ? $siteData->footer_copyright : "Footer copyright text Not set yet.";
+        $siteTagline = !empty($siteData->tagline) ? $siteData->tagline : "";
+        View::share('siteName', $siteTitle);
+        View::share('sitelogo', $sitelogo);
+        View::share('footerCopyright', $footer_copyright);
+        View::share('siteTagline', $siteTagline);
+    }
+}
---
+++ resources/views/templates/master.blade.php
@@ -0,0 +1,62 @@
+<x-header/>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+{{--  @include('templates/preloader')  --}}
+<x-preloader />
+{{--  @include('templates/navbar')  --}}
+<x-navbar />
+{{--  @include('templates/sidebar')  --}}
+<x-sidebar />
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">@yield('title')</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            @yield('pagename')
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+  <!-- Main content -->
+  <section class="content">
+
+
+
+
+
+ @yield('main_content')
+
+
+
+
+
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+
+{{--  @include('templates/footer')  --}}
+<x-footer />
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<x-scripts/>
+{{--  <x-scripts />  --}}
+</body>
+</html>
